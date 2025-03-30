851ceabd41c7b69a2cc0952aa8208549f0310c71
ksoft7 <olukayodesulieman@gmail.com>
2025-03-30 23:54:22 +0100

    updated

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,29 +1,27 @@
-
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-    
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        // required: true,
+      type: String,
     },
     verify: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     otp: {
-        type: String,
+      type: String,
     },
 
     otpCreatedat: {
-        type: Date,
-        
+      type: Date,
     },
-
-}, {
+  },
+  {
     timestamps: true,
-}); 
+  }
+);
 export default mongoose.model("User", userSchema);
