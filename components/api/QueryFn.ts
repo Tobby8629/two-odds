ab89de4c561348e9981be441a1635ca5a02f60b3
tobby8629 <sarhmullah15@gmail.com>
2026-07-12 23:44:41 -0500

    Delete backend files

--- components/api/QueryFn.ts
+++ components/api/QueryFn.ts
@@ -5,12 +5,11 @@ export const postQuery = async (url: string, data: any) => {
   try {
     const response = await postRequest(url, data)
     if (response.status === 200 || response.status === 201) {
-        // console.log("Response data:", response.data);
-        return response.data;
-      }
-      throw new Error(`Request failed with status ${response.status}`);
+      return response.data;
+    }
+    throw new Error(`Request failed with status ${response.status}`);
   }
-   catch (error) {
+  catch (error) {
     console.error('Error:', error);
     if (axios.isAxiosError(error)) {
       console.error("Axios error message:", error.message);
